3037e2ef96b042607950d96ad01db474f1938c4e
Kajah Cunningham <kajahcunningham@gmail.com>
2019-10-14 14:33:54 -0400

    added turns

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const gridDivArray = document.querySelectorAll(".grid")
+const playerMoves= []
+let turn = "x"
 
-for(let i =0; i < gridDivArray.length; i++){
-const squareBtn = document.createElement("button");
-gridDivArray[i].appendChild(squareBtn);
-// squareBtn.addEventListener('click', function(){
-
-// })
-
-}
+ for (let i =0; i < gridDivArray.length; i++){
+    gridDivArray[i].addEventListener("click", function(){
+        const divIndex = this.id;
+        playerMoves[divIndex] = turn; 
+        console.log(playerMoves);
+        this.innerText=turn   
+        turn = (turn === "x") ? "o" : "x"
+        console.log(turn)
+        
+    })
+ }
