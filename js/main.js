d2ec5d648d3ca361d721e4e0a097dbb0622a88ab
Kajah Cunningham <kajahcunningham@gmail.com>
2019-10-14 11:21:51 -0400

    grid created

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const gridDivArray = document.querySelectorAll(".grid")
+
+for(let i =0; i < gridDivArray.length; i++){
+const squareBtn = document.createElement("button");
+gridDivArray[i].appendChild(squareBtn);
+// squareBtn.addEventListener('click', function(){
+
+// })
+
+}
