b9cb23e276b9dfbee73c9401f9bb7cc051ec3187
Kajah Cunningham <kajahcunningham@gmail.com>
2019-10-16 09:18:53 -0400

    updated styling

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Halloween-tic-tac-toe
 
-Instructions: The objective of Tic-Tac-Toe is to place three of your marks in a horizontal, vertical, or diagonal row on a 3x3 grid. Players will alternate between X(ghost) or O(pumpkins). The player who accomplishes the objective is the winner. 
+Instructions: The objective of Tic-Tac-Toe is to place three of your marks in a horizontal, vertical, or diagonal row on a 3x3 grid. Players will alternate between X or O. The player who accomplishes the objective is the winner. If neither player accomplishes the objective, there isn't a winner and the match is a draw.
+
+User stories:
+
+As a user I want to enjoy a playable game so that I challenge others.
+As a user I want to play tic tac to so that I can pass time.
--- js/main.js
+++ js/main.js
@@ -1,15 +1,94 @@
 const gridDivArray = document.querySelectorAll(".grid")
 const playerMoves= []
+let winner = ''
 let turn = "x"
 
+
+// for loop to look through each box//
  for (let i =0; i < gridDivArray.length; i++){
     gridDivArray[i].addEventListener("click", function(){
         const divIndex = this.id;
         playerMoves[divIndex] = turn; 
         console.log(playerMoves);
-        this.innerText=turn   
+        this.innerText = turn   
         turn = (turn === "x") ? "o" : "x"
-        console.log(turn)
-        
-    })
+        checkWinner() //WHY DO WE CALL THE FUNCTION HERE?
+        // console.log(turn)
+        // How do we keep the turns from being able to be switched ? SOLVED
+
+    }, {once: true})
+
  }
+ 
+ function checkWinner() {
+
+ //compare moves array to solutions
+        //solution1
+       if (playerMoves[0] === playerMoves[1] && playerMoves[1] === playerMoves[2] ){   
+        winner = playerMoves[0]
+        console.log(`${winner} is the winner`);
+        //alert(`${winner} is the winner`)
+       }
+        //solution2
+        else if (playerMoves[3] === playerMoves[4] && playerMoves[4] === playerMoves[5] ){
+            winner = playerMoves[3]
+            console.log(`${winner} is the winner`)
+        }
+        //solution3
+        else if (playerMoves[6] === playerMoves[7] && playerMoves[7] === playerMoves[8] ){
+            winner = playerMoves[6]
+            console.log(`${winner} is the winner`)}
+
+        //solution4
+        else if (playerMoves[0] === playerMoves[3] && playerMoves[3] === playerMoves[6] ){
+           winner = playerMoves[0]
+           console.log(`${winner} is the winner`)}
+
+        //solution5
+        else if (playerMoves[1] === playerMoves[4] && playerMoves[4] === playerMoves[7] ){
+            winner = playerMoves[1]
+            console.log(`${winner} is the winner`)}
+
+        //solution6
+        else if (playerMoves[2] === playerMoves[5] && playerMoves[5] === playerMoves[8] ){
+            winner = playerMoves[2]
+            console.log(`${winner} is the winner`)
+        }
+        //solution7
+        else if (playerMoves[0] === playerMoves[4] && playerMoves[4] === playerMoves[8] ){
+            winner = playerMoves[0]
+            console.log(`${winner} is the winner`)
+        }
+        //solution8
+        else if (playerMoves[2] === playerMoves[4] && playerMoves[4] === playerMoves[6] ){
+            winner = playerMoves[2]
+            console.log(`${winner} is the winner`)
+        }
+    }
+
+    // How do we move the message onto the screen instead of the console.log?
+    // HOW CAN I CHANGE THE FONT OF THE X'S & O'S?
+    // HOW DO I TELL WHOSE TURN IT IS ?Can i display this?
+
+ //loop through all 8 solutions to find winner (TURNS OUT WE DONT NEED A LOOP)
+//  for (i = 0; i < playerMoves.length; i++){
+
+ 
+ //winning solutions: [0,1,2], [3,4,5] [6,7,8] [0,3,6] [1,4,7] [2,5,8], [0,4,8] [2,4,6] 
+
+ //button attempts
+ // btn = document.querySelector("button")
+ // HOW DO I MAKE A RESET BUTTON THAT ACTUALLY WORKS?
+    //Can you help me finish setting my buttons?
+const newGameButton = document.querySelector("new-game-button")
+button.addEventListener("click",function(){
+
+}) 
+const resetButton = document.querySelector("reset-button")
+button.addEventListener("click",function(){
+
+}) 
+ //  HOW DO I STRIKE THROUGH THE WINNING SOLUTION(3 X'S OR O'S)?
+    //  is this how (this.style.textDecoration = 'line-through';)?
+//
+  
